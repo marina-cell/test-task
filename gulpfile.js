102bac79b0aa89dc1ea5244349dd4afeee69f71c
marina_cell <melissa_nsk_@mail.ru>
2018-12-12 21:28:50 +0700

    Базовая разметка и стилизация страницы проекта: графика, шрифты, фон

--- gulpfile.js
+++ gulpfile.js
@@ -58,7 +58,7 @@ gulp.task("html", function () {
 gulp.task("copy", function () {
   return gulp.src([
     "source/fonts/**/*.{woff,woff2}",
-    "source/img/**",
+    "source/img/*.{png,jpg}",
     "source/js/**"
   ], {
     base: "source"
